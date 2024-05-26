17adbbb627b11ba4e073b310b40f6452a37d20bb
garnicamatias <garnicamatias02@gmail.com>
2024-05-26 13:15:18 -0300

    Login Form Component

--- front/src/app/login/components/login-form/login-form.component.ts
+++ front/src/app/login/components/login-form/login-form.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCard} from '@angular/material/card'
+import { BtnPrimaryComponent } from '../../../shared/components/btn-primary/btn-primary.component';
+import { BtnSecondaryComponent } from '../../../shared/components/btn-secondary/btn-secondary.component';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatIconModule, MatCard, BtnPrimaryComponent, BtnSecondaryComponent],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
-export class LoginFormComponent {
 
+
+export class LoginFormComponent {
+  hide = true;
+  clickEvent(event: MouseEvent) {
+    this.hide = !this.hide;
+    event.stopPropagation();
+  }
 }
